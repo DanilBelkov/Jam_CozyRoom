6d54455bad056bcdf3e937c812c3aaefa992c404
Данил Белков <dan120699@mail.ru>
2024-06-30 21:37:08 +0500

    Стартовое меню

--- Assets/Scripts/StartMenuManager.cs
+++ Assets/Scripts/StartMenuManager.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartMenuManager : MonoBehaviour
 {
 
+    [SerializeField]
+    private List<GameObject> _Pages;
+
+    private GameObject _Page;
+
+    private void Awake()
+    {
+        _Page = _Pages.First();
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene("test", LoadSceneMode.Single);
+    }
+
+    public void NextPage()
+    {
+        _Page.SetActive(false);
+        _Page = _Pages[_Pages.IndexOf(_Page) + 1];
     }
 }
